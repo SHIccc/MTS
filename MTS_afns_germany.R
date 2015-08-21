7f8d7383476069e87be463767bbbb2e89232ba0a
Shi Chen <chenshiq@hu-berlin.de>
2015-08-21 16:06:45 +0200

    format of R updated

--- MTS_afns_germany.R
+++ MTS_afns_germany.R
@@ -8,7 +8,7 @@
 #              and predicted state variables.
 # ------------------------------------------------------------------------------
 # Keywords:    Kalman filter, optimization, MLE, maximum likelihood, bond, plot,
-#              filter, estimation, extrapolation, dynamics, term structure,
+#              filter, estimation, extrapolation, dynamics, term structure
 #              interest-rate
 # ------------------------------------------------------------------------------
 # See also:    
@@ -21,7 +21,7 @@ rm(list = ls(all = TRUE))
 graphics.off()
 
 ## install and load packages
-libraries = c("zoo", "FKF", "expm", "Matrix")
+libraries = c("zoo", "FKF")
 lapply(libraries, function(x) if (!(x %in% installed.packages())) {
   install.packages(x)
 })
@@ -51,9 +51,8 @@ for (i in 2:length(demat)) {
 
 y51 = t(dejoi)
 
-yieldadj_joint = function(sigma11, sigma12 = 0, sigma13 = 0, sigma21 = 0, 
-                           sigma22, sigma23 = 0, sigma31 = 0, sigma32 = 0, sigma33, sigma44, lambda, 
-                           time = maturity) {
+yieldadj_joint = function(sigma11, sigma12 = 0, sigma13 = 0, sigma21 = 0, sigma22, sigma23 = 0, 
+                          sigma31 = 0, sigma32 = 0, sigma33, sigma44, lambda, time = maturity) {
   Atilde = sigma11^2 + sigma12^2 + sigma13^2 + sigma44^2
   Btilde = sigma21^2 + sigma22^2 + sigma23^2
   Ctilde = sigma31^2 + sigma32^2 + sigma33^2
@@ -62,30 +61,29 @@ yieldadj_joint = function(sigma11, sigma12 = 0, sigma13 = 0, sigma21 = 0,
   Ftilde = sigma21 * sigma31 + sigma22 * sigma32 + sigma23 * sigma33
   
   adj1 = Atilde * time^2/6
-  adj2 = Btilde * (1/(2 * lambda^2) - (1 - exp(-lambda * time))/(lambda^3 * 
-                                                                    time) + (1 - exp(-2 * time * lambda))/(3 * lambda^3 * time))
-  adj3 = Ctilde * (1/(2 * lambda^2) + exp(-lambda * time)/(lambda^2) - 
-                      time * exp(-2 * lambda * time)/(4 * lambda) - 3 * exp(-2 * lambda * 
-                                                                              time)/(4 * lambda^2) - 2 * (1 - exp(-lambda * time))/(lambda^3 * 
-                                                                                                                                      time) + 5 * (1 - exp(-2 * lambda * time))/(8 * lambda^3 * time))
-  adj4 = Dtilde * (time/(2 * lambda) + exp(-lambda * time)/(lambda^2) - 
-                      (1 - exp(-lambda * time))/(lambda^3 * time))
-  adj5 = Etilde * (3 * exp(-lambda * time)/(lambda^2) + time/(2 * lambda) + 
-                      time * exp(-lambda * time)/lambda - 3 * (1 - exp(-lambda * time))/(lambda^3 * 
-                                                                                           time))
-  adj6 = Ftilde * (1/(lambda^2) + exp(-lambda * time)/(lambda^2) - exp(-2 * 
-                                                                          lambda * time)/(2 * lambda^2) - 3 * (1 - exp(-lambda * time))/(lambda^3 * 
-                                                                                                                                           time) + 3 * (1 - exp(-2 * lambda * time))/(4 * lambda^3 * time))
-  
+  adj2 = Btilde * (1/(2 * lambda^2) - (1 - exp(-lambda * time))/(lambda^3 * time) 
+                   + (1 - exp(-2 * time * lambda))/(3 * lambda^3 * time))
+  adj3 = Ctilde * (1/(2 * lambda^2) + exp(-lambda * time)/(lambda^2) 
+                   - time * exp(-2 * lambda * time)/(4 * lambda)
+                   - 3 * exp(-2 * lambda * time)/(4 * lambda^2) 
+                   - 2 * (1 - exp(-lambda * time))/(lambda^3 * time) 
+                   + 5 * (1 - exp(-2 * lambda * time))/(8 * lambda^3 * time))
+  adj4 = Dtilde * (time/(2 * lambda) + exp(-lambda * time)/(lambda^2) 
+                   - (1 - exp(-lambda * time))/(lambda^3 * time))
+  adj5 = Etilde * (3 * exp(-lambda * time)/(lambda^2) + time/(2 * lambda) 
+                   + time * exp(-lambda * time)/lambda - 3 * (1 - exp(-lambda * time))/(lambda^3 * time))
+  adj6 = Ftilde * (1/(lambda^2) + exp(-lambda * time)/(lambda^2) 
+                   - exp(-2 * lambda * time)/(2 * lambda^2) 
+                   - 3 * (1 - exp(-lambda * time))/(lambda^3 * time) 
+                   + 3 * (1 - exp(-2 * lambda * time))/(4 * lambda^3 * time))
   return(adj1 + adj2 + adj3 + adj4 + adj5 + adj6)
 }
 
 Meloading_joint = function(lambda, alphaS, alphaC, time = maturity) {
-  row1 = c(1, (1 - exp(-lambda * time))/(lambda * time), (1 - exp(-lambda * 
-                                                                     time))/(lambda * time) - exp(-lambda * time), 0)
+  row1 = c(1, (1 - exp(-lambda * time))/(lambda * time), 
+           (1 - exp(-lambda * time))/(lambda * time) - exp(-lambda * time), 0)
   row2 = c(0, alphaS * ((1 - exp(-lambda * time))/(lambda * time)), 
-            alphaC * ((1 - exp(-lambda * time))/(lambda * time) - exp(-lambda * 
-                                                                        time)), 1)
+            alphaC * ((1 - exp(-lambda * time))/(lambda * time) - exp(-lambda * time)), 1)
   MatrixB = rbind(row1, row2)
   return(MatrixB)
 }
@@ -94,13 +92,13 @@ afnsss = function(t1, t2, t3, t4, t5, t6, t7, t8, t9, t10, t11, t12, t13,
                    t14, t15, t16, s1, s2, s3, s4, g1, g2, l1, h1, h2, h3, h4) {
   Tt = matrix(c(t1, t2, t3, t4, t5, t6, t7, t8, t9, t10, t11, t12, t13, 
                  t14, t15, t16), nr = 4)
-  Zt = rbind(Meloading_joint(lambda = l1, alphaS = g1, alphaC = g2, 
-                              time = demat[1]), Meloading_joint(lambda = l1, alphaS = g1, alphaC = g2, 
-                                                                time = demat[2]), Meloading_joint(lambda = l1, alphaS = g1, alphaC = g2, 
-                                                                                                  time = demat[3]))
+  Zt = rbind(Meloading_joint(lambda = l1, alphaS = g1, alphaC = g2, time = demat[1]), 
+              Meloading_joint(lambda = l1, alphaS = g1, alphaC = g2, time = demat[2]), 
+              Meloading_joint(lambda = l1, alphaS = g1, alphaC = g2, time = demat[3]))
   ct = matrix(rep(c(yieldadj_joint(sigma11 = h1, sigma22 = h2, sigma33 = h3, 
-                                    sigma44 = h4, lambda = l1, time = demat[1]), yieldadj_joint(sigma11 = h1, 
-                                                                                                sigma22 = h2, sigma33 = h3, sigma44 = h4, lambda = l1, time = demat[2]), 
+                                    sigma44 = h4, lambda = l1, time = demat[1]), 
+                     yieldadj_joint(sigma11 = h1, sigma22 = h2, sigma33 = h3, 
+                                    sigma44 = h4, lambda = l1, time = demat[2]), 
                      yieldadj_joint(sigma11 = h1, sigma22 = h2, sigma33 = h3, sigma44 = h4, 
                                     lambda = l1, time = demat[1])), each = 2), nr = 6, nc = 1)
   dt = matrix(c(1 - t1, t2, t3, t4, t5, 1 - t6, t7, t8, t9, t10, 1 - 
@@ -115,7 +113,6 @@ afnsss = function(t1, t2, t3, t4, t5, t6, t7, t8, t9, t10, t11, t12, t13,
               HHt = HHt))
 }
 
-
 objective = function(theta, yt) {
   sp = afnsss(theta["t1"], theta["t2"], theta["t3"], theta["t4"], theta["t5"], 
               theta["t6"], theta["t7"], theta["t8"], theta["t9"], theta["t10"], 
@@ -128,9 +125,9 @@ objective = function(theta, yt) {
   return(-ans$logLik)
 }
 
-theta = c(t = c(0.8, 0, 0, 0, 0, 0.8, 0, 0, 0, 0, 0.8, 0, 0, 0, 0, 0.8), 
-           s = c(0.2, 0.2, 0.2, 0.2), g = c(0.6, 0.6), l1 = c(0.7), h = c(0.2, 
-                                                                          0.2, 0.2, 0.2))
+theta = c(t = c(0.7, 0, 0, 0, 0, 0.7, 0, 0, 0, 0, 0.7, 0, 0, 0, 0, 0.7), 
+          s = c(0.3, 0.3, 0.3, 0.3), g = c(0.6, 0.6), l1 = c(0.7), 
+          h = c(0.2, 0.2, 0.2, 0.2))
 
 fit = optim(theta, objective, yt = y51, hessian = TRUE)
 sp = afnsss(fit$par["t1"], fit$par["t2"], fit$par["t3"], fit$par["t4"], 
@@ -151,7 +148,7 @@ save(joide0915fit, file = "joide0915fit.RData")
 ## The plots of filtered and predicted state variables Another approach:
 ## plot.fkf(ans, CI=NA)
 plot(ans$at[1, -1], type = "l", col = "red", ylab = "State variables", 
-     xlab = "", ylim = c(-10, 6), lwd = 2)
+     xlab = "", ylim = c(-8, 6), lwd = 2)
 lines(ans$att[1, -1], lty = 2, col = "red", lwd = 2)
 lines(ans$at[2, -1], lty = 1, col = "purple", lwd = 2)
 lines(ans$att[2, -1], lty = 2, col = "purple", lwd = 2)
