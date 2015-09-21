4c7f2524bb1439b3e10ed33be4d5572127415950
Shi Chen <chenshiq@hu-berlin.de>
2015-09-21 15:18:13 +0200

    s

---
+++ Moivre-Laplace.R
@@ -0,0 +1,42 @@
+# ---------------------------------------------------------------------
+# Project:      Bachelor Thesis
+# ---------------------------------------------------------------------
+# Quantlet:     BCS_CLT_StandBinpdf
+# ---------------------------------------------------------------------
+# Description:  Produces a histogram of a standardized binomial 
+#               distribution on behalf of empirical samples and shows 
+#               the possible approximation for large n
+#               throughout the standard normal distribution  
+# ---------------------------------------------------------------------
+# Output:       Histogram ("n100.png")
+# ---------------------------------------------------------------------
+# Keywords:     approximation, standardized binomial, distribution, 
+#               graphical representation, visualization, histogram, pdf,
+#               CLT, moivre-laplace
+# ---------------------------------------------------------------------
+# Author:       Fabrice Wunderlich,  2015/08/01
+# ---------------------------------------------------------------------
+
+
+
+rm(list = ls(all = TRUE))
+graphics.off()
+
+# Parameter settings of the binomial random variable and of the sample size N
+n = 100
+p = 0.2
+N = 1000000
+
+# Sample of the N stadardized binomial random variables
+bsample = (rbinom(N, n, p) - (n * p))/(sqrt(n * p * (1 - p)))
+
+# Plot 
+breakingpoints = seq(min(bsample), max(bsample), length.out = 40)
+hist(bsample, freq = FALSE, breaks = breakingpoints, xlab = "(k-np)/sqrt(np(1-p))", 
+     ylab = "b(n,p;k)", xlim = c(min(bsample - 1), max(bsample + 1)),
+     main = paste("n=", n, ", p=", p))
+
+x = seq(min(bsample - 1), max(bsample + 1), by = 0.02)
+norm = dnorm(x, 0, 1)
+lines(x, norm, col = "dark red")
+
