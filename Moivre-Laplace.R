844c493509a13641ae26a15c9c7721ada81bddbf
Shi Chen <chenshiq@hu-berlin.de>
2015-10-15 11:09:47 +0200

    change date

--- Moivre-Laplace.R
+++ Moivre-Laplace.R
@@ -14,14 +14,14 @@
 #               graphical representation, visualization, histogram, pdf,
 #               CLT, moivre-laplace
 # ---------------------------------------------------------------------
-# Author:       Fabrice Wunderlich,  2015/08/01
+# Author:       Fabrice Wunderlich,  2015/10/01
 # ---------------------------------------------------------------------
 
 
 
 # Clear loaded variables and close graphics
 closeAllConnections()
-rm(list=ls(all=TRUE))
+rm(list = ls(all = TRUE))
 graphics.off()
 
 # Parameter settings of the binomial random variable and of the sample size N
@@ -30,13 +30,14 @@ p = 0.2
 N = 1000000
 
 # Sample of the N stadardized binomial random variables
-bsample = (rbinom(N,n,p)-(n*p))/(sqrt(n*p*(1-p)))
+bsample = (rbinom(N, n, p) - (n * p))/(sqrt(n * p * (1 - p)))
 
 # Plot 
-breakingpoints = seq(from=min(bsample-1),to=max(bsample+1),by=0.25)
-hist(bsample,freq=FALSE,breaks=breakingpoints,xlab="(k-np)/sqrt(np(1-p))",ylab="b(n,p;k)",xlim=c(min(bsample-1),max(bsample+1)),main=paste("n=",n,", p=",p))
+breakingpoints = seq(from = min(bsample - 1), to = max(bsample + 1), by = 0.25)
+hist(bsample, freq = FALSE, breaks = breakingpoints, xlab = "(k-np)/sqrt(np(1-p))", 
+     ylab = "b(n,p;k)", xlim = c(min(bsample - 1), max(bsample + 1)), 
+     main = paste("n=", n, ", p=", p))
 
-x     = seq(min(bsample-1),max(bsample+1), by=0.02)
-norm  = dnorm(x,0,1)
-lines(x,norm,col="dark red")
-
+x = seq(min(bsample - 1), max(bsample + 1), by = 0.02)
+norm = dnorm(x, 0, 1)
+lines(x, norm, col = "dark red")
